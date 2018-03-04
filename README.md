013da711424a25309cdea4b6cfeba51dc078f4d9
yijaejun <yijaejun81@gmail.com>
2018-03-05 08:16:52 +0900

    no message

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # chat-example
 
-##1.Nodejs + express + socket.io + reactjs
-##2.Karma-Mocha
+##1.Nodejs + express + socket.io + reactjs + Nodemon
+##2.Karma-Mocha + webpack + vue.js
 ##3.MongoDB - Mongoose
 ##4.로그인 기능 + 지난 채팅 내용확인 가능 + 특정인과 채팅하기
 ##5.나에게 누가 말을 걸면 알람을 뜨게 하기 (특정 브라우저 알람 기능 지원시만 / 혹은 채팅창이 반짝이게 한다)
 
- 
 
 
 
--- index.js
+++ index.js
@@ -8,9 +8,13 @@ app.get('/', function(req, res){
 });
 
 // 소켓연결 구문이 겨우 이정도에서 끝
+// 클라이언트에서 chatMsg로 방출한 메시지를 서버에서 받고
+// 클라이언트에서도 받는다
+// 서버에서 받아주는 구문이 없이는 클라이언트에서 보여줄 수가 없다.
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) =>{
-    io.emit('chat message', msg);
+  socket.on('chatMsg', (msg) =>{
+  	console.log(msg);
+    io.emit('chatMsg', msg);
   });
 });
 
